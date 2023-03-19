7a02d89711a5aaac39fee0713bd488df5231e3dd
Sampurna Atmaramani <s.atmaramani@gmail.com>
2023-03-19 16:35:50 +0530

    How to change port in React JS
    How to change port in React JS - from default port 3000 to 4000 Or anything you want

---
+++ How to Change port in React JS/src/pages/Home.js
@@ -0,0 +1,10 @@
+import "../App.css";
+const Home = ()=>{
+
+    return <><div className="App-header"> Port change app = ? Inside the env file (.env) I have provided the changed port number <br />  
+        i.e. PORT=4000 instead of default port 3000 - hence this react JS app will run on http://localhost:4000
+    <br /></div></>
+   
+}
+
+export default Home;
